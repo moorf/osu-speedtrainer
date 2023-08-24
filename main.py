8a05ac42d5539fbec6c2a26600760bd4f6662c5f
moorf <47717776+moorf@users.noreply.github.com>
2023-08-24 04:09:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ start_time = 0
 error = 0
 b1 = 'r'
 b2 = 't'
+bpm = 180
 late = 0
 restart = True
 milliseconds = 0
@@ -20,18 +21,18 @@ os.system('color 3')
 
 punishmentms = 30
 
-b1 = 't'
-b2 = 'r'
+config_filename = "config.cfg"
 
 quitbutton = 'q'
-restartbutton = 'z'
+restartbutton = '`'
 bpmbutton = 'p'
 
-bpm = 180
+
     
 def main():
-    global fail, hook, time_window, bpm, restart,start, clicks, alternate, alternate2, start_time, error
+    global fail, hook, time_window, bpm, restart,start, clicks, alternate, alternate2, start_time, error,b1,b2,punishmentms
     
+    read_config(config_filename)
     
     time_window = (60 / (bpm * 4)) * 1000
     
@@ -40,8 +41,7 @@ def main():
         if restart:
             os.system('clear')
             print(f"Press the buttons alternately at {bpm} BPM. You have {time_window:.2f} ms to press the next button. Buttons: {b1} {b2}.")
-            
-            
+                
             clicks = 0
             alternate = 0
             alternate2 = 0
@@ -49,7 +49,6 @@ def main():
             start = 0
             start_time = 0
             error = 0
-            
             
             hook = keyboard.on_press(check_for_keys)
             restart = False
@@ -67,7 +66,7 @@ def restarter(e):
             restart = True
         
         if e.name == bpmbutton:
-            bpmchange()
+            bpmchange()     
             
 def check_for_keys(e):
     global clicks, alternate, alternate2, fail, start, time_window, start_time, error, b1,b2, late, restart, hook, quitbutton, restartbutton
@@ -125,10 +124,16 @@ def erroroutput():
                
 
 def bpmchange():
-    global bpm, time_window
+    global bpm, time_window, config_filename
     bpm = get_bpm()
     time_window = (60 / (bpm * 4)) * 1000
+    with open(config_filename, 'r') as file:
+        lines = file.readlines()
+        lines[2] = str(bpm) + '\n'
+    with open(config_filename, 'w') as file:
+        file.writelines(lines)
     print(f"Saved. BPM is {bpm}. {quitbutton} to quit,  {restartbutton} to restart.  {bpmbutton} to change bpm.")
+    
     
 def get_bpm():
     try:
@@ -141,6 +146,32 @@ def get_bpm():
             return bpm
     except ValueError:
         print("Invalid input. Please enter a valid integer.")
+
+def create_default_config(filename):
+    with open(filename, 'w') as f:
+        f.write("e\n")
+        f.write("r\n")
+        f.write("180\n")
+        f.write("30\n")
+        f.write("q\n")
+        f.write("`\n")
+        f.write("p\n")
+
+def read_config(filename):
+    global b1, b2, bpm, punishmentms, quitbutton, restartbutton, bpmbutton
+    try:
+        with open(filename, 'r') as f:
+            lines = f.readlines()
+            b1 = lines[0].strip()
+            b2 = lines[1].strip()
+            bpm = int(lines[2].strip())
+            punishmentms = int(lines[3].strip())
+            quitbutton = lines[4].strip()
+            restartbutton = lines[5].strip()
+            bpmbutton = lines[6].strip()
+    except FileNotFoundError:
+        print(f"Config file '{filename}' not found. Creating a new one with default values.")
+        create_default_config(filename)
 
 def cleanup():
     global hook
